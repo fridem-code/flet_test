3b6bb9d805e33d78606a6794cef08901c356dac0
fridem <nik_dantexxl@mail.ru>
2025-10-25 17:18:00 +0300

    add base structure

--- app_1.py
+++ app_1.py
@@ -3,7 +3,21 @@ import flet as ft
 
 def app(page: ft.Page):
     page.title = 'App1'
-    page.add(ft.Text('Привет!'))
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    body = ft.Container(
+        content=ft.Column(
+            [
+                ft.Text('Привет!', size=36, text_align=ft.TextAlign.CENTER),
+                ft.Button('Нажми меня')
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        ),
+        alignment=ft.alignment.center
+    )
+
+    page.add(body)
 
 
 ft.app(app)
